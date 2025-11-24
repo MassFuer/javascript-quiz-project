0611dfd590601c01afe76f7062c9e479085117d6
MassFuer <maxime.ducamp@gmail.com>
2025-11-24 17:10:44 +0100

    Quiz Day-1

--- src/question.js
+++ src/question.js
@@ -1,7 +1,21 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices; // array
+    this.answer = answer;
+    this.difficulty = difficulty; // number between 1 and 3
+  }
+  // 2. shuffleChoices()
 
-    // 2. shuffleChoices()
+  // [1, 2, 3 , 4] ==>
+  shuffleChoices() {
+    this.choices.sort(() => Math.random() - 0.5);
+  }
 }
+
+// question = new Question("text", [4, 2, 1, 3], "answer", 3);
+// console.log(question);
+// console.log(question.shuffleChoices);
